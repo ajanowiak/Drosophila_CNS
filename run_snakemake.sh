2d108d0640dfae130bcaaf7d1183c8cdbb0dc740
olek <olekjanowiak916@gmail.com>
2026-05-26 21:49:16 +0200

    Reorganize repository into project template layout

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-# Run the pipeline locally, using 32 cores
-snakemake --snakefile src/snakemake/test.Snakefile --use-conda -j 32 -k
+snakemake \
+    --snakefile src/snakemake/Snakefile \
+    --cores 1 \
+    --printshellcmds \
+    --reason
